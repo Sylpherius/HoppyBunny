0541ffdc39749ada3492f9406ccd2b6841f58249
Alan Huang <alanyuhuang@gmail.com>
2015-06-24 13:16:14 -0700

    Finished Make School's Game Of Life tutorial.

---
+++ GameOfLife.spritebuilder/Creature.swift
@@ -0,0 +1,24 @@
+//
+//  Creature.swift
+//  GameOfLife
+//
+//  Created by Alan on 6/24/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Creature: CCSprite {
+    
+    var isAlive = false {
+        didSet {
+            self.visible = isAlive
+        }
+    }
+    var livingNeighborsCount = 0
+    
+    convenience override init() {
+        self.init(imageNamed: "GameOfLifeAssets/Assets/bubble.png")
+        anchorPoint = CGPoint(x: 0, y: 0)
+    }
+}
