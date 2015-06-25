a11d0fd7dcad8d5a8f4dccbd73f22dd16622c0f0
Alan Huang <alanyuhuang@gmail.com>
2015-06-25 10:33:23 -0700

    Added catapult mechanics

--- PeevedPenguins.spritebuilder/Gameplay.swift
+++ PeevedPenguins.spritebuilder/Gameplay.swift
@@ -12,7 +12,10 @@ class Gameplay: CCNode {
     weak var gamePhysicsNode : CCPhysicsNode!
     weak var catapultArm : CCNode!
     weak var levelNode : CCNode!
-    weak var contentNode: CCNode!
+    weak var contentNode : CCNode!
+    weak var pullbackNode : CCNode!
+    weak var mouseJointNode: CCNode!
+    var mouseJoint: CCPhysicsJoint?
     
     // called when CCB file has completed loading
     func didLoadFromCCB() {
@@ -21,13 +24,44 @@ class Gameplay: CCNode {
         levelNode.addChild(level)
         // visualize physics bodies & joints
         gamePhysicsNode.debugDraw = true
+        // nothing shall collide with our invisible nodes
+        pullbackNode.physicsBody.collisionMask = []
+        mouseJointNode.physicsBody.collisionMask = []
     }
     
     // called on every touch in this scene
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-        launchPenguin()
+        let touchLocation = touch.locationInNode(contentNode)
+        
+        // start catapult dragging when a touch inside of the catapult arm occurs
+        if CGRectContainsPoint(catapultArm.boundingBox(), touchLocation) {
+            // move the mouseJointNode to the touch position
+            mouseJointNode.position = touchLocation
+            
+            // setup a spring joint between the mouseJointNode and the catapultArm
+            mouseJoint = CCPhysicsJoint.connectedSpringJointWithBodyA(mouseJointNode.physicsBody, bodyB: catapultArm.physicsBody, anchorA: CGPointZero, anchorB: CGPoint(x: 34, y: 138), restLength: 0, stiffness: 3000, damping: 150)
+        }
     }
-    
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        // whenever touches move, update the position of the mouseJointNode to the touch position
+        let touchLocation = touch.locationInNode(contentNode)
+        mouseJointNode.position = touchLocation
+    }
+    func releaseCatapult() {
+        if let joint = mouseJoint {
+            // releases the joint and lets the catapult snap back
+            joint.invalidate()
+            mouseJoint = nil
+        }
+    }
+    override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        // when touches end, meaning the user releases their finger, release the catapult
+        releaseCatapult()
+    }
+    override func touchCancelled(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
+        releaseCatapult()
+    }
     func launchPenguin() {
         // loads the Penguin.ccb we have set up in SpriteBuilder
         let penguin = CCBReader.load("Penguin") as! Penguin
